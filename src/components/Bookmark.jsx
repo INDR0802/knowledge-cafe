20e4846209c8bc37a0fdee0973820c7e8034ab78
Indrajit Karmaker <indrajitkarmaker87@gmail.com>
2024-03-13 21:37:19 +0600

    test

--- src/components/Bookmark.jsx
+++ src/components/Bookmark.jsx
@@ -1,6 +1,6 @@
 const Bookmark = () => {
   return (
-    <div className="border-2 w-2/3 bg-slate-300 p-8 rounded-2xl h-full">
+    <div className="border-2 w-2/3 bg-slate-300 p-8 rounded-2xl">
       <h1 className="text-2xl font-extrabold">Bookmarked Blogs: </h1>
     </div>
   );
